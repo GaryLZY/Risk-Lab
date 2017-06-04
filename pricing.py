d02908e3c5116929214db88f44eef7786ba90543
alexEpis <alexEpis@users.noreply.github.com>
2017-06-04 12:57:01 -0400

    Draft for the pricing class

---
+++ pricing.py
@@ -0,0 +1,23 @@
+
+class pricing
+
+    def option(self, ):
+        # Prices a call/put option
+        pass
+
+    def imp_vol(self):
+        # returns the implied volatility of an option
+        pass
+
+    def bond(self):
+        # Prices a bond
+        pass
+
+    def default_prob(self):
+        # Calculate default probabilities
+        pass
+
+    def cds(self):
+        # Price credit default swaps
+        pass
+
